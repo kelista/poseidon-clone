3c3d0c33166f25c1bfc3832e08af05937382576d
Joshua Hendrawan <joshuahendrawan03@gmail.com>
2020-07-30 02:03:59 +0700

    reverting src got deleted

---
+++ src/styles/base.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingLeft: 18,
+    paddingRight: 18
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center"
+  }
+});
