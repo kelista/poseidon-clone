b5f5cb8786a598e7b3f5b9251594c59f6af0f781
kelista <kevinchristiankelista@gmail.com>
2020-08-14 21:06:27 +0700

    Account BGM stat, biggerSelection btn

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -10,23 +10,23 @@ export const BottomNavigation = ({home, setting} : { home: Function, setting: Fu
       <LinearGradient colors={['#6E0000', '#400000']} style={styles.navigationLinear}>
         <View style={styles.navigationWrapper}>
           <View style={styles.navigationButton}>
-            <TouchableOpacity onPress={() => home()}>
-              <Image source={require('../assets/images/others/home-nav.png')}/>
+            <TouchableOpacity style={styles.buttonSelection} onPress={() => home()}>
+              <Image style={[styles.imgButton, styles.homeNavImg]} source={require('../assets/images/others/home-nav.png')}/>
             </TouchableOpacity>
           </View>
           <View style={styles.navigationButton}>
-            <TouchableOpacity>
-              <Image source={require('../assets/images/others/report-nav.png')} />
+            <TouchableOpacity style={styles.buttonSelection} >
+              <Image style={[styles.imgButton, styles.reportNavImg]} source={require('../assets/images/others/report-nav.png')} />
             </TouchableOpacity>
           </View>
           <View style={styles.navigationButton}>
-            <TouchableOpacity>
-              <Image source={require('../assets/images/others/mail-nav.png')} />
+            <TouchableOpacity style={styles.buttonSelection} >
+              <Image style={[styles.imgButton, styles.mailNavImg]} source={require('../assets/images/others/mail-nav.png')} />
             </TouchableOpacity>
           </View>
           <View style={styles.navigationButtonLast}>
-            <TouchableOpacity onPress={() => setting()}>
-              <Image source={require('../assets/images/others/setting-nav.png')} />
+            <TouchableOpacity style={styles.buttonSelection} onPress={() => setting()}>
+              <Image style={[styles.imgButton, styles.settingNavImg]} source={require('../assets/images/others/setting-nav.png')} />
             </TouchableOpacity>
           </View>
         </View>
@@ -65,6 +65,40 @@ const styles = StyleSheet.create({
     height: '100%',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  buttonSelection: {
+    width: '100%',
+    height: '100%',
+    position: 'relative'
+  },
+  imgButton: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%'
+  },
+  homeNavImg: {
+    transform: [
+      {translateX: -11},
+      {translateY: -10}
+    ]
+  },
+  reportNavImg: {
+    transform: [
+      {translateX: -8},
+      {translateY: -10}
+    ]
+  },
+  mailNavImg: {
+    transform: [
+      {translateX: -13.5},
+      {translateY: -8.5}
+    ]
+  },
+  settingNavImg: {
+    transform: [
+      {translateX: -10},
+      {translateY: -10}
+    ]
   }
 });
 
